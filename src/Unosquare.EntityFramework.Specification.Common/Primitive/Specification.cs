a97d4ca1ce9329a8f43233baa84eda8ddf88bfc1
Geovanni Perez <1775792+geoperez@users.noreply.github.com>
2022-12-12 16:05:37 -0600

    Review

--- src/Unosquare.EntityFramework.Specification.Common/Primitive/Specification.cs
+++ src/Unosquare.EntityFramework.Specification.Common/Primitive/Specification.cs
@@ -4,30 +4,24 @@ public abstract class Specification
 {
     public abstract Expression GetExpression();
 }
-    
+
 public abstract class Specification<T> : Specification
 {
     protected static Expression<Func<T, bool>> ShowAll => x => true;
-        
+
     protected static Expression<Func<T, bool>> ShowNone => x => false;
 
     public abstract Expression<Func<T, bool>> BuildExpression();
 
-    public bool IsSatisfy(T item)
-    {
-        return BuildExpression().Compile()(item);
-    }
-        
-    public override Expression GetExpression()
-    {
-        return BuildExpression();
-    }
+    public bool IsSatisfy(T item) => BuildExpression().Compile()(item);
+
+    public override Expression GetExpression() => BuildExpression();
 }
-    
+
 public abstract class Specification<T, TU> : Specification
 {
     protected static Expression<Func<T, TU, bool>> ShowAll => (x, y) => true;
-        
+
     protected static Expression<Func<T, TU, bool>> ShowNone => (x, y) => false;
 
     public abstract Expression<Func<T, TU, bool>> BuildExpression();
@@ -38,19 +32,12 @@ public abstract class Specification<T, TU> : Specification
     public override Expression GetExpression() =>
         BuildExpression();
 }
-    
+
 public class ExpressionSpec<T> : Specification<T>
 {
-    private readonly Expression<Func<T, bool>> _exp;
+    private readonly Expression<Func<T, bool>>? _exp;
 
-    public ExpressionSpec(Expression<Func<T, bool>> exp = null)
-    {
-        _exp = exp;
-    }
-    public override Expression<Func<T, bool>> BuildExpression()
-    {
-        if (_exp == null) return ShowAll;
-            
-        return _exp;
-    }
+    public ExpressionSpec(Expression<Func<T, bool>>? exp = null) => _exp = exp;
+
+    public override Expression<Func<T, bool>> BuildExpression() => _exp ?? ShowAll;
 }
